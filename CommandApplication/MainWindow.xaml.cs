b3f338ce93fac9114d39a57c55edfc52846bfaba
steinar <stein@LENOVO>
2018-01-14 03:06:50 +0100

    Add project files.

---
+++ CommandApplication/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Google.Maps;
+using Google.Maps.StaticMaps;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CommandApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            GoogleSigned.AssignAllServices(new GoogleSigned("AIzaSyDE-w-vFJxyw5W4HGqK435n46mSdNGUEys"));
+            InitializeComponent();
+            string curDir = Directory.GetCurrentDirectory();
+            //mapNavi.Navigate(new Uri(String.Format("file:///{0}/Views/openlayermap.html", curDir)));
+            //var map = new StaticMapRequest();
+            //map.Center = new Location("1600 Pennsylvania Ave NW, Washington, DC 20500");
+            //map.Size = new System.Drawing.Size(400, 400);
+            //map.Zoom = 14;
+            
+            //map.Scale = 2;
+            //mapImage.Source = new BitmapImage(map.ToUri());
+
+            
+
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var video = new VideoWindow();
+            video.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var sensor = new SensorWindow();
+            sensor.Show();
+        }
+    }
+}
---
+++ CommandApplication/Model/ShipAndVoyageData.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandApplication.Model
+{
+    class ShipAndVoyageData
+    {
+        public string MessageType { get; set; }
+        public int IMO { get; set; }
+        public string CallSign { get; set; }
+        public string ShipName { get; set; }
+        public string ShipType { get; set; }
+        public double ToBow { get; set; }
+        public double ToStern { get; set; }
+        public double ToStarboard { get; set; }
+        public double ToPort { get; set; }
+        public string PositionFixingDevice { get; set; }
+        public string ETA { get; set; }
+        public double Draught { get; set; }
+        public string Destination { get; set; }
+        public string DataTerminalReady { get; set; }
+        public ShipAndVoyageData(string[] inpArray)
+        {
+            MessageType = extractString(inpArray[0]);
+            
+            CallSign = extractString(inpArray[2]);
+            ShipName = extractString(inpArray[3]);
+            ShipType = extractString(inpArray[4]);
+            IMO = extractInt(inpArray[1]);
+            ToBow = extractDouble(inpArray[5]);
+            ToStern = extractDouble(inpArray[6]);
+            ToStarboard = extractDouble(inpArray[7]);
+            ToPort = extractDouble(inpArray[8]);
+            PositionFixingDevice = extractString(inpArray[9]);
+            ETA = extractString(inpArray[10]);
+            Draught = extractDouble(inpArray[11]);
+            Destination = extractString(inpArray[12]);
+            DataTerminalReady = extractString(inpArray[13]);
+        }
+        private string extractString(string s)
+        {
+            if (s.Contains("'"))
+            {
+                int from = s.IndexOf("='") + "='".Length;
+                int to = s.Length - 1;
+                s = s.Substring(from, to - from);
+            }
+            else
+            {
+                int from = s.IndexOf("=") + 1;
+                int to = s.Length;
+                s = s.Substring(from, to - from);
+            }
+            return s;
+        }
+        private int extractInt(string s)
+        {
+            if(!s.Contains("IMO"))
+            {
+                int from = s.IndexOf("=") + 1;
+                int to = s.Length;
+                s = s.Substring(from, to - from);
+            }
+            else
+            {
+                int from = s.IndexOf("[imo=") + "[imo=".Length;
+                int to = s.Length - 1;
+                s = s.Substring(from, to - from);
+            }
+            return int.Parse(s);
+        }
+        private double extractDouble(string s)
+        {
+            int from = s.IndexOf("=") + 1;
+            int to = s.Length;
+            s = s.Substring(from, to - from);
+
+            return Convert.ToDouble(s, CultureInfo.InvariantCulture);
+        }
+    }
+}
